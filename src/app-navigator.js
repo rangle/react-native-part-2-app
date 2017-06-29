90d4d90920e0e6872a2df252a7311541ab46439c
Varun Vachhar <varunvachhar@gmail.com>
2017-06-29 09:38:16 -0400

    starting point

--- src/app-navigator.js
+++ src/app-navigator.js
@@ -3,16 +3,18 @@ import { addNavigationHelpers, StackNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 import { routeConfig } from './route-config';
 
+import { LoginScreen } from './screens';
+
 
 /**
  * AppNavigator
  */
-export const AppNavigator = // ¯\_(ツ)_/¯
+// export const AppNavigator = // ¯\_(ツ)_/¯
 
 
 // Connect AppNavigator to redux
-const App = // ¯\_(ツ)_/¯
+// const App = // ¯\_(ツ)_/¯
 
 
 // Get the navigator state
-export const AppWithNavigationState = // ¯\_(ツ)_/¯
+export const AppWithNavigationState = () => <LoginScreen />;
