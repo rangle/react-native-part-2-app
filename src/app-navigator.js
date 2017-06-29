a4e9ff95394b890e3a1462bb079200a3b8dd66c7
Varun Vachhar <varunvachhar@gmail.com>
2017-06-29 09:49:42 -0400

    🚧

--- src/app-navigator.js
+++ src/app-navigator.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { addNavigationHelpers, StackNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
-import { routeConfig } from './route-config';
+import { LoginScreen, MainScreen, ProfileScreen } from './screens';
 
+
+LoginScreen.navigationOptions = { title: 'Login' };
+MainScreen.navigationOptions = { title: 'Main' };
+ProfileScreen.navigationOptions = { title: 'Profile' };
 
 /**
  * AppNavigator
  */
-export const AppNavigator = StackNavigator(routeConfig);
+export const AppNavigator = StackNavigator({
+  Login: { screen: LoginScreen },
+  Main: { screen: MainScreen },
+  Profile: { screen: ProfileScreen },
+});
 
 
 // Connect AppNavigator to redux
